2c0ac96f041a6e7248d4c49fd431aa850a795a80
grzesmich <michal.grzesik@orange.com>
2020-02-05 13:22:28 +0100

    test docker-compose - v 0.9

--- task.py
+++ task.py
@@ -1,14 +1,15 @@
 from celery import Celery
 
 CELERY_BROKER_URL = 'amqp://rabbitmq:rabbitmq@rabbit:5672/'
-CELERY_RESULT_BACKEND = 'rpc://'
+# CELERY_RESULT_BACKEND = 'rpc://'
 # Initialize Celery
-celery = Celery('worker', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+celery = Celery('worker', broker=CELERY_BROKER_URL)#, backend=CELERY_RESULT_BACKEND)
 
 
 @celery.task
 def times_task(x, y):
     return x * y
+
 
 @celery.task
 def add_task(x, y):
